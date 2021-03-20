ecbf3e2086bc281b8ec6dd306668a7b5f4193d71
x0s3 <x0s3.prog@gmail.com>
2021-03-20 11:04:45 +0100

    Expose produceSchema util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './composeSchema';
 export * from './renderSchema';
+export * from './produceSchema';
 export * from './insertSection';
 export * from './insertField';
 export * from './removeField';
